98ab8ca73a1196577e215da9e59de9e707087e68
shan1y <76669891+shan1y@users.noreply.github.com>
2022-12-18 00:44:24 +0000

    Sync LeetCode submission - Remove Element (javascript)

---
+++ my-folder/problems/remove_element/solution.js
@@ -0,0 +1,11 @@
+var removeElement = function(nums, val) {
+    let length = nums.length;
+    for(let i=0; i < length; i++){
+
+        if(nums[i]===val){
+            nums.splice(i,1)
+            i--;
+        }
+    }
+    return nums.length;
+};
